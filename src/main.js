81d186491ea21c03251dd17cf14b560e0a4ca7b6
Henrique Chaves <henriquechavesmm@gmail.com>
2021-10-26 15:35:56 -0300

    Test Husky hook (Sucess)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const a = 3;
-const b = 2
+const b = 2;
 const c = '38797893289732879329782343420ioasdjhkasdhjkjsdhkjfkhsdhjkdsfkhjsdfhkjdfskhjsfdhkjfsdkhjsdfhkjdsfhkjsdfhkjdfshjikdfs';
 
 function sum() {
