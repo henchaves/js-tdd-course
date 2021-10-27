91bab45d4b9fea13830f9bb67cc419616662bf7a
Henrique Chaves <henriquechavesmm@gmail.com>
2021-10-27 15:16:21 -0300

    Add webpack

--- src/main.js
+++
@@ -1,9 +0,0 @@
-const a = 3;
-const b = 2;
-const c = '38797893289732879329782343420ioasdjhkasdhjkjsdhkjfkhsdhjkdsfkhjsdfhkjdfskhjsfdhkjfsdkhjsdfhkjdsfhkjsdfhkjdfshjikdfs';
-
-function sum() {
-  return a + b + c;
-}
-
-sum();
